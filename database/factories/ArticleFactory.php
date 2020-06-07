fc6168661be62ae87b1362d34ca368026e968b32
Samir El Asri <samirel733@gmail.com>
2020-06-07 20:55:09 +0100

    Introducing database seeding using the Faker library
    Faker is an open library for seeding the database with fake test data, relying on Seeder and Factory classes, and offering a wide range of properties to create dummy data for.

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Article;
+use Faker\Generator as Faker;
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'user_id'=>17,
+        'title'=>$faker->catchPhrase(),
+        'content'=>implode($faker->paragraphs(3)),
+        'feature'=>'noimage.jpg',
+        'category_id'=>$faker->numberBetween(2,8),
+        'created_at'=>\Carbon\Carbon::now(),
+        'updated_at'=>\Carbon\Carbon::now()
+    ];
+});
